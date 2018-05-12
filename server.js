17ef004c0c7d9cd0e0f5a06650d20d5dd12ec018
Sahil <sahil.dada@gmail.comgit config --global user.email sahil.dada@gmail.com>
2018-05-12 09:44:43 +0000

    last push

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-
-
+const Gpio = require('onoff').Gpio;
+var ledR = new Gpio(23, 'out');
+var ledY = new Gpio(27, 'out');
+var ledG = new Gpio(22, 'out');
+var interval= null;
+ledR.writeSync(0);
+ledY.writeSync(0);
+ledG.writeSync(1);
+var interval = null;
 var express = require('express');
 // var https = require('https');
 var http = require('http');
@@ -16,37 +23,77 @@ app.use(bodyParser.json());
 
 var routes = require('./publicroutes');
 routes(app);
-var devID = abcxyz123rt;
-
-
+var devID = "abcxyz123rt";
+var emitClick;
+var disableClick = false;
+var blinkLEDSend = function() {
+	ledR.writeSync(1);
+	ledY.writeSync(0);
+	ledG.writeSync(0)
+}
+var blinkLEDAck = function() {
+	ledR.writeSync(0);
+	ledY.writeSync(1);
+	ledG.writeSync(0)
+}
+var blinkLEDApr = function() {
+	ledR.writeSync(0);
+	ledY.writeSync(0);
+	ledG.writeSync(1);
+}
 var WebSocketClient = require('websocket').client;
 var client = new WebSocketClient();
 
 client.on('connectFailed', function(error) {
     console.log('Connect Error: ' + error.toString());
+	ledR.writeSync(1);
+	ledY.writeSync(1);
+	ledG.writeSync(1);
 });
  
 client.on('connect', function(connection) {
+clearInterval(interval); console.log('reconnected'); disableClick = false ; blinkLEDApr();
     console.log('WebSocket Client Connected');
     connection.on('error', function(error) {
         console.log("Connection Error: " + error.toString());
     });
+
 connection.on('close', function() {
         console.log('echo-protocol Connection Closed');
+	ledR.writeSync(1);
+	ledY.writeSync(0);
+	ledG.writeSync(0);
+	
+	interval = setInterval(function(){
+		console.log('reconnect');
+	ledR.writeSync(1);
+	ledY.writeSync(1);
+	ledG.writeSync(1);
+		client.connect('wss://node-db-api.run.aws-usw02-pr.ice.predix.io/wsinit');
+		} , 10000)
     });
 connection.on('message', function(message) {
+	
         if (message.type === 'utf8') {
             console.log("Received: '" + message.utf8Data + "'");
+		var messagedata = JSON.parse(message.utf8Data);
+		if(messagedata.type === 'ACKNOWLEDGED') {disableClick=true ; blinkLEDAck() }
+		else if(messagedata.type === 'APPROVED') {disableClick=false ; blinkLEDApr() }
+		
+		
         }
     });
     
-   const emitClick = function () {
+emitClick = function () {
         if (connection.connected) {
             var msg = {};
 	    msg.type = "IOT";
             msg.author = devID
             msg.payload = {timestamp : new Date().getTime()}
+	    //msg.payload = devID + " pinged";
+		console.log(JSON.stringify(msg));
             connection.sendUTF(JSON.stringify(msg));
+		disableClick=true ; blinkLEDSend()
             //setTimeout(sendNumber, 1000);
         }
     }
@@ -54,11 +101,12 @@ connection.on('message', function(message) {
 });
 client.connect('wss://node-db-api.run.aws-usw02-pr.ice.predix.io/wsinit');
 // GPIO 
-const Gpio = require('onoff').Gpio;
-const button = new Gpio(17, 'in', 'rising' , {debounceTimeout: 10});
+
+const button = new Gpio(17, 'in', 'rising' , {debounceTimeout: 100});
 button.watch(function (err, value) {
-	console.log(err);
+	// console.log(err);
   console.log(value);
+if(value === 1 && !disableClick){emitClick()}
 });
  
 
